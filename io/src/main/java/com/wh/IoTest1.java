92ce716676b8a2d35f7da9859b73caa91e103114
whbanner <china_wh@163.com>
2020-01-17 00:00:24 +0800

    io

--- io/src/main/java/com/wh/IoTest1.java
+++ io/src/main/java/com/wh/IoTest1.java
@@ -55,4 +55,23 @@ public class IoTest1 {
             System.out.print(c);
         }
     }
+    @Test
+    public void  test12() throws IOException {
+        // 输入和输出都使用缓冲流
+        FileInputStream in = new FileInputStream("E:\\视频资料\\大数据原理与应用\\1.1大数据时代.mp4");
+        BufferedInputStream inBuffer = new BufferedInputStream(in);
+        FileOutputStream out = new FileOutputStream("1.1大数据时代.mp4");
+        BufferedOutputStream outBuffer = new BufferedOutputStream(out);
+        int len = 0;
+        byte[] bs = new byte[1024];
+        long begin = System.currentTimeMillis();
+        while ((len = inBuffer.read(bs)) != -1) {
+            outBuffer.write(bs, 0, len);
+        }
+        System.out.println("复制文件所需的时间：" + (System.currentTimeMillis() - begin)); // 平均时间约 200 多毫秒
+        inBuffer.close();
+        in.close();
+        outBuffer.close();
+        out.close();
+    }
 }
