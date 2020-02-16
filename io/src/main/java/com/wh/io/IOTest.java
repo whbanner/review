84690d02a715098f03c0a90bec323c6d154587c5
whbanner <china_wh@163.com>
2020-02-16 23:39:28 +0800

    io nio 比较

---
+++ io/src/main/java/com/wh/io/IOTest.java
@@ -0,0 +1,74 @@
+package com.wh.io;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * https://blog.csdn.net/forezp/article/details/88414741
+ */
+public class IOTest {
+    /**
+     * nio
+     */
+    public static void method1(){
+        RandomAccessFile aFile = null;
+        try{
+            aFile = new RandomAccessFile("src/nio.txt","rw");
+            FileChannel fileChannel = aFile.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            int bytesRead = fileChannel.read(buf);
+            System.out.println(bytesRead);
+            while(bytesRead != -1)
+            {
+                buf.flip();
+                while(buf.hasRemaining())
+                {
+                    System.out.print((char)buf.get());
+                }
+                buf.compact();
+                bytesRead = fileChannel.read(buf);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(aFile != null){
+                    aFile.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * io
+     */
+    public static void method2(){
+        InputStream in = null;
+        try{
+            in = new BufferedInputStream(new FileInputStream("src/nomal_io.txt"));
+            byte [] buf = new byte[1024];
+            int bytesRead = in.read(buf);
+            while(bytesRead != -1)
+            {
+                for(int i=0;i<bytesRead;i++)
+                    System.out.print((char)buf[i]);
+                bytesRead = in.read(buf);
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }finally{
+            try{
+                if(in != null){
+                    in.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
