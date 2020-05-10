0581f5665a71753f132c522fb892adba38bb2543
whbanner <china_wh@163.com>
2020-05-10 23:16:14 +0800

    springboot-first for jar

---
+++ springboot/src/main/java/com/example/Main1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //标注是个springboot应用
+public class Main1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main1Application.class,args);
+    }
+}
